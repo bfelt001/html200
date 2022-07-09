112fecf0f880a7ea9f291fa3d6ed2e7800731a66
Breanne Felton <breanne.felton@gmail.com>
2022-07-09 16:34:34 -0700

    initial add

---
+++ lesson02/calculator.js
@@ -0,0 +1,19 @@
+let v = 2;
+let x = 4;
+let y = 3;
+let z = 10;
+
+let add = v+x+y+z;
+console.log(add);
+
+let result = x*y/v;
+console.log(result);
+
+let power = y**z;
+console.log(power);
+
+let div = x/y%z;
+console.log(div);
+
+let one = v++*v;
+console.log(one);
